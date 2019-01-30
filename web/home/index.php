7fc78c6b834cd165ce78b2a295187ff3bc2378fe
orange <jambonbill@gmail.com>
2019-01-30 18:29:21 +0800

    +$B=new Djang\Base("../../profiles/127.0.0.1.json");

--- web/home/index.php
+++ web/home/index.php
@@ -5,9 +5,18 @@ session_start();
 
 require __DIR__."/../../vendor/autoload.php";
 
-$B=new Djang\Base;
+$B=new Djang\Base("../../profiles/127.0.0.1.json");
 
 
+//echo "User #".$B->userId()."<br />";
+
+$B->logout();
+
+$user=$B->user();
+
+echo "<pre>";print_r($user);echo "</pre>";
+
+exit("ok");
 
 if(!$B->isStaff()){
   header('location:../login');
