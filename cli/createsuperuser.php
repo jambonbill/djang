014a0aeeea6deffaf10c8022e93c2311bf8e56f3
orange <jambonbill@gmail.com>
2019-02-04 01:19:06 +0800

    +$username =	readline("Enter Username: ");

--- cli/createsuperuser.php
+++ cli/createsuperuser.php
@@ -2,20 +2,24 @@
 session_start();
 require __DIR__."/../vendor/autoload.php";
 
-//$B=new Djang\Base(__DIR__."/../profiles/127.0.0.1.json");
+$B=new Djang\Base(__DIR__."/../profiles/127.0.0.1.json");
+$User=new Djang\User($B);
 
-/*
-$Models=new Djang\Models(__DIR__."/../profiles/127.0.0.1.json");
-echo "Djang\Models->createTables()\n";
-$Models->createSuperUser();
-*/
+//print_r($B->config());exit;
 
-$email = readline("Enter email: ");
-$username = readline("Enter Username: ");
-$first_name = readline("Enter first name: ");
-$last_name = readline("Enter last name: ");
-$password = readline("Enter Password: ");
-$password2 = readline("Confirm password: ");
+
+
+$email=		readline("Enter email: ");
+
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  exit("Invalid email format\n");
+}
+
+$username =	readline("Enter Username: ");
+$first_name=readline("Enter first name: ");
+$last_name=	readline("Enter last name: ");
+$password=	readline("Enter Password: ");
+$password2=	readline("Confirm password: ");
 
 if ($password!=$password2) {
 	exit("Error: passwords dont match\n");
@@ -30,14 +34,29 @@ echo "--------------\n";
 
 $confirm = readline("Confirm create superuser? (y/n): ");
 
-//get 3 commands from user
-
-/*
-for ($i=0; $i < 3; $i++) {
-        $line = readline("Command: ");
-        readline_add_history($line);
+if ($confirm!="y") {
+	echo "bye\n";
 }
-*/
+
+$uid=$User->create($email, $first_name, $last_name);
+
+echo "Superuser #uid created successfully\n";
+
+if($User->updatePassword($uid, $password)){
+	echo "Password updated\n";
+}
+
+if($User->activate($uid)){
+	echo "user activated\n";
+}else{
+	exit("error: not activated");
+}
+
+// Make superuser //
+$sql="UPDATE auth_user SET is_superuser=1 WHERE id=$uid LIMIT 1;";
+echo "$sql\n";
+$B->db()->query($sql) or die("Error: $sql\n");
+
 
 //dump history
 //print_r(readline_list_history());
