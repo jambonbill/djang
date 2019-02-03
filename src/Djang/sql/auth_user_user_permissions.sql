026eab0668ebac1eadbf2bc6273221089ed75801
orange <jambonbill@gmail.com>
2019-02-04 01:25:57 +0800

    Djang

---
+++ src/Djang/sql/auth_user_user_permissions.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `auth_user_user_permissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_user_user_permissions_user_id_permission_id_14a6b632_uniq` (`user_id`,`permission_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
