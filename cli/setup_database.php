c9fdb6c24111de189411998b8ff61e88c62c52aa
orange <jambonbill@gmail.com>
2019-02-04 01:26:09 +0800

    up

--- cli/setup_database.php
+++ cli/setup_database.php
@@ -2,30 +2,11 @@
 session_start();
 require __DIR__."/../vendor/autoload.php";
 
-$B=new Djang\Base(__DIR__."/../profiles/127.0.0.1.json");
-
 $Models=new Djang\Models(__DIR__."/../profiles/127.0.0.1.json");
 
 echo "Djang\Models->createTables()\n";
 
 $Models->createTables();
-
-$Users=new Djang\User($B);
-
-$users=[];
-$users[]='staff@example.com';
-$users[]='student@example.com';
-
-foreach($users as $email){
-
-	$uid=$Users->create($email, 'staff', 'example');
-
-	//$Users->create('student@example.com', 'student', 'example');
-
-	$Users->activate($uid);
-
-}
-
 
 
 // TODO //
