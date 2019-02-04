870708cdd7884a0094134af256aaafd67bb97f38
orange <jambonbill@gmail.com>
2019-02-05 00:18:48 +0800

    - `middle_name`

--- src/Djang/sql/auth_user.sql
+++ src/Djang/sql/auth_user.sql
@@ -5,7 +5,6 @@ CREATE TABLE IF NOT EXISTS `auth_user` (
  `is_superuser` tinyint(1) DEFAULT '0',
  `username` varchar(30) NOT NULL,
  `first_name` varchar(30) DEFAULT NULL,
- `middle_name` varchar(30) DEFAULT NULL,
  `last_name` varchar(30) DEFAULT NULL,
  `email` varchar(254) NOT NULL,
  `is_staff` tinyint(1) DEFAULT '0',
